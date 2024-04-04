bd15d206396c7dbd600f2c36ad20405fd23e2f94
iankisali <iankisali@gmail.com>
2024-04-04 11:18:13 +0300

    changing image tag

--- script.groovy
+++ script.groovy
@@ -11,7 +11,7 @@ def buildJar() {
 def buildImage() {
     echo "building docker image.."
     withCredentials([usernamePassword(credentialsId: 'dockerhub-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t iankisali/demo-repo:jma-1.2 .'
+        sh 'docker build -t iankisali/demo-repo:java-app-v1.1 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh 'docker push iankisali/demo-repo:java-app-v1.1'
     }
