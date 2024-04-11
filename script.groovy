735536a251098f636ad3084877dca0df947621c1
iankisali <iankisali@gmail.com>
2024-04-11 12:37:40 +0300

    update groovy script and Jenkinsfile with buildjar and buildimage

--- script.groovy
+++ script.groovy
@@ -3,6 +3,20 @@ def test() {
     echo "testing the application .."
 }
 
+def buildJar() {
+    echo "building the application .."
+    sh 'mvn package'
+}
+
+def buildImage() {
+    echo "building docker image.."
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t iankisali/demo-repo:jma-1.2 .'
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh 'docker push iankisali/demo-repo:jma-1.2'
+    }
+} 
+
 def deploy() {
     echo "deploying app.."
 }
